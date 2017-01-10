39f87d1b4cfeb8de9b70c66f40ff2d096b9dd3f0
Lucas <lucas.o@live.com>
2017-01-10 17:15:06 -0200

    new post

---
+++ blog/atualizacao-equipe-agosto/index.php
@@ -0,0 +1,134 @@
+
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <title>Atualização da equipe</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="robots" content="index,follow,archive">
+    <meta property="og:locale" content="en_us">
+    <meta property="og:title" content="Team update">
+    <meta property="og:url" content="https://openai.com/blog/team-update-august/">
+    <meta property="og:site_name" content="OpenAI">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content=" We've hired more great people to help us achieve our goals. Welcome, everyone! 
+
+">    
+
+    
+    <meta property="og:image" content="https://openai.com/assets/social-8a6bde705feb6c4073a948e6aa1fd346bb34d02677d4a699c9f173f7060b57b1.jpg">
+    
+    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image:width" content="1400">
+    <meta property="og:image:height" content="700">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@openai">
+    <meta name="twitter:url" content="https://openai.com/blog/team-update-august/">
+    <meta name="twitter:title" content="Team update">
+    <meta name="twitter:description" content=" We've hired more great people to help us achieve our goals. Welcome, everyone! 
+
+">
+    
+    
+    <meta name="twitter:image" content="https://openai.com/assets/social-8a6bde705feb6c4073a948e6aa1fd346bb34d02677d4a699c9f173f7060b57b1.jpg">
+
+
+    <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/1b9ff7da-9bc0-4849-80e5-eeee396a0b17.css"/>
+    <link type="text/css" rel="stylesheet" href="https://openai.com/assets/shared-34b0931c40d8e24dc7b7dbe1db15e28061648524c809a05cfa824d983b0e9271.css">
+    
+    
+    <link rel="icon" href="https://openai.com/assets/favicon-81f16c8d5fcc93e1b02354b72b40abb680deb71081fa467625154ccf471883dc.ico">
+    <link rel="shortcut icon" href="https://openai.com/assets/favicon-81f16c8d5fcc93e1b02354b72b40abb680deb71081fa467625154ccf471883dc.ico">
+    <link rel="apple-touch-icon" href="https://openai.com/assets/favicon-b52807aa8dda8e2badc657cccbdd1d44f388941a3c10522ba682098881d7c4ed.png">
+
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="https://openai.com/feed.xml" />
+</head>
+
+  <body class=''>
+
+    
+
+    
+<?php include '../../header.html.php'; ?>
+
+
+<div class='Post'>
+<div class="container">
+  <div class="row
+              center-xs">
+
+  <div class="col-xs-12
+              col-sm-11
+              col-md-8
+              col-lg-8">
+      
+        <div class="Post-header">
+          <h1 class='Post-title'>Atualização da equipe</h1>
+          <div class='Post-meta'>
+            <span class="Post-author">Jie Tang e Greg Brockman</span>
+            <span class="Post-date">16 de Agosto de 2016</span>
+          </div>
+        </div>
+        <p> <a href="https://jobs.lever.co/openai">Contratamos</a> mais pessoas incríveis para nos ajudar a atingir nossos <a href="/blog/objetivos-tecnicos-openai">objetivos</a>. Sejam todos bem-vindos! </p>
+
+<hr>
+
+<h2> Tempo integral </h2>
+
+<ul class='HiresList'>
+    
+    
+  <li><strong>Dario Amodei.</strong> <a href="https://www.linkedin.com/in/dario-amodei-3934934">Dario</a> foi um dos autores principais do <a href="https://arxiv.org/abs/1512.02595">Deep Speech 2</a>, um sistema de fala que alcançou desempenho quase humano em muitas tarefas de fala. Ele também é co-autor principal do “<a href="https://arxiv.org/abs/1606.06565">Problemas Concretos de Segurança em IA</a>”, que destaca questões relacionadas a acidentes em sistemas de machine learning. Antes da OpenAI, ele trabalhou no Google Brain.</li>
+    
+  <li><strong>Filip Wolski</strong> O passado recente de <a href="https://www.linkedin.com/in/filip-wolski-a7273728">Filip</a> é em modelagem "prática", tendo passado os últimos anos trabalhando na area de negociação de alta frequência. No passado ele gostava de resolver problemas em competições de programação, e ganhou o <a href="http://stats.ioinformatics.org/people/768">IOI</a> e <a href="https://icpc.baylor.edu/community/history-icpc-2007">ACM ICPC</a>.</li>
+    
+  <li><strong>Jack Clark.</strong> <a href="https://twitter.com/jackclarksf">Jack</a> passou os últimos anos escrevendo sobre inteligência artificial e sistemas distribuídos, mais recentemente na Bloomberg e BusinessWeek. Seus artigos abordaram tecnologias como <a href="http://www.bloomberg.com/news/features/2015-06-09/the-future-of-computers-is-the-mind-of-a-toddler">redes de memória</a>, <a href="http://www.bloomberg.com/news/articles/2015-12-02/computers-learn-how-to-paint-whatever-you-tell-them-to">geração de imagens</a>, e <a href="http://www.bloomberg.com/features/2015-preschool-for-robots/">reinforcement learning para robôs</a>, e questões como a <a href="http://www.bloomberg.com/news/articles/2016-06-23/artificial-intelligence-has-a-sea-of-dudes-problem">diversidade dentro da IA</a>. Como nosso Diretor de Estratégia e Comunicação, ele ajudará com o alcance comunitário, políticas, comunicações e estratégia.</li>
+
+  <li><strong>Scott Gray.</strong> <a href="https://twitter.com/scottgray76">Scott</a> foi anteriormente um engenheiro na <a href="https://www.nervanasys.com/author/scott-g/">Nervana Systems</a> onde se concentrou em otimizar o desempenho de deep networks em GPUs. Suas <a href="http://arxiv.org/abs/1509.09308">otimizações em assembly-level</a> para álgebra linear e convolução densa continuam sendo as mais rápidas disponíveis. Quando não está <a href="https://github.com/scott-gray">escrevendo software</a>, costuma gastar seu tempo lendo as pesquisas mais atuais sobre neurociência e campos relacionados.</li>
+    
+  <li><strong>Zain Shah.</strong> <a href="http://twitter.com/zan2434">Zain</a> anteriormente liderou esforços em deep learning para construir um sistema inteligente humano-máquina colaborativo na <a href="http://claralabs.com">Clara Labs</a>. Ele também trabalhou em <a href="http://cepstral.com">síntese de fala</a> e <a href="http://cnbc.cmu.edu">neurociência computacional</a>, construiu o <a href="http://mosaic.io">Mosaic</a>, e fundou uma <a href="http://watchsend.com">empresa</a> de análise comportamental mobile. Ele construiu recentemente um <a href="http://deepgif.tarzain.com">motor de busca de GIF</a> usando deep multimodal embeddings.</li>
+
+</ul>
+
+
+<hr>
+      
+<h2> Estagiários &amp; Visitantes </h2>
+
+<p> Também estamos estimulados a trabalhar com as seguintes pessoas por um período de tempo mais limitado: </p>
+
+<ul class='HiresList'>
+        
+  <li> <strong>Catherine Olsson</strong>. <a href="https://www.linkedin.com/in/catherineolsson">Catherine</a> construiu a <a href="https://github.com/openai/gym-http-api">API REST</a> do OpenAI Gym, que já atraiu usuários de <a href="https://github.com/openai/gym-http-api/tree/master/binding-lua">Lua</a>, <a href="https://github.com/openai/gym-http-api/tree/master/binding-cpp">C++</a>, <a href="https://github.com/deeplearning4j/gym-java-client">Java</a>, e <a href="https://github.com/openai/gym-http-api/tree/master/binding-rust">Rust</a>. Ela se formou com um GPA perfeito em Ciencia da Computação e Ciência Cognitiva e Científica no MIT, e tem vasta experiência em pesquisa de <a href="http://f1000research.com/posters/1098032">neurociência computacional</a> e <a href="https://scholar.google.com/citations?view_op=view_citation&hl=en&user=TvdMDhwAAAAJ&citation_for_view=TvdMDhwAAAAJ:2osOgNQ5qMEC">psicologia</a>. Catherine ensinou <a href="http://www.cns.nyu.edu/~eero/math-tools14/">programação e matemática aplicada</a> por seis anos, alcançando mulheres e minorias subrepresentadas. </li>
+
+
+  <li><strong>Harri Edwards</strong> <a href="https://github.com/harri-edwards">Harri</a> é um estudante de doutorado na University of Edinburgh, onde ele está pesquisando modelos que podem rapidamente se adaptar a novas situações <a href="https://arxiv.org/abs/1606.02185">aprendendo a representar conjuntos de dados</a>.</li>
+
+    
+  <li><strong>Igor Mordatch.</strong> <a href="https://people.eecs.berkeley.edu/~igor.mordatch/">Igor</a> está interessado em controle ótimo, machine learning, e suas aplicações para robótica, biomecânica e neurociência. Seu doutorado foi em descoberta automatizada e aprendizagem de <a href="https://www.youtube.com/watch?v=mhr_jtQrhVA">comportamentos</a> de movimento complexos.  Ele se juntará à faculdade em <a href="http://www.cmu.edu/">CMU</a> em setembro de 2017.</li>
+    
+    
+    
+  <li><strong>Taco Cohen</strong> <a href="http://ta.co.nl/">Taco</a> é um estudante de doutorado que trabalha em problemas aplicados e teóricos em representation learning. Recentemente ele inventou a group equivariant convolutional neural networks (<a href="http://jmlr.org/proceedings/papers/v48/cohenc16.pdf">G-CNNs</a>), uma generalização de CNNs que melhora a eficiência estatística destes modelos explorando simetrias.</li>
+    
+    
+  <li><strong>Tambet Matiisen</strong> <a href="https://github.com/tambetm/">Tambet</a> é um estudante de doutorado da University of Tartu, na Estonia. Anteriormente trabalhou como engenheiro de software e fundou sua própria <a href="http://www.korteriyhistu.net/">startup</a>. Seus projetos recentes variam de <a href="http://arxiv.org/abs/1511.08779">fazer agentes de deep reinforcement learning côoperarem</a> para predizer a posição de um rato a partir de suas atividades cerebrais. Ele também escreveu uma <a href="http://neuro.cs.ut.ee/demystifying-deep-reinforcement-learning/">introdução acessível à deep Q-learning</a>.</li>
+
+</ul>
+
+<hr>
+
+        
+      </div>
+      
+    </div>
+  </div>
+</div>
+</div>
+
+    
+
+<?php include '../../scripts.html.php'; ?>
+  </body>
+</html>
