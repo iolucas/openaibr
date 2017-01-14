a2923977089401c2083c78d2fd539da2aaac07ed
Lucas <lucas.o@live.com>
2017-01-14 12:08:06 -0200

    new post

---
+++ blog/machine-learning-unconference/index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en-us">
+  <head>
+    <title>Machine Learning Unconference</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+    <meta name="robots" content="index,follow,archive">
+    <meta property="og:locale" content="en_us">
+    <meta property="og:title" content="Machine Learning Unconference">
+    <meta property="og:url" content="https://openai.com/blog/machine-learning-unconference/">
+    <meta property="og:site_name" content="OpenAI">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="  The latest information about the Unconference is now available
+  at the Unconference wiki,
+which will be periodically updated with more information for attendees.
+
+
+
+">    
+
+    
+    <meta property="og:image" content="https://openai.com/assets/social-8a6bde705feb6c4073a948e6aa1fd346bb34d02677d4a699c9f173f7060b57b1.jpg">
+    
+    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image:width" content="1400">
+    <meta property="og:image:height" content="700">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@openai">
+    <meta name="twitter:url" content="https://openai.com/blog/machine-learning-unconference/">
+    <meta name="twitter:title" content="Machine Learning Unconference">
+    <meta name="twitter:description" content="  The latest information about the Unconference is now available
+  at the Unconference wiki,
+which will be periodically updated with more information for attendees.
+
+
+
+">
+    
+    
+    <meta name="twitter:image" content="https://openai.com/assets/social-8a6bde705feb6c4073a948e6aa1fd346bb34d02677d4a699c9f173f7060b57b1.jpg">
+
+
+    <link type="text/css" rel="stylesheet" href="//fast.fonts.net/cssapi/1b9ff7da-9bc0-4849-80e5-eeee396a0b17.css"/>
+    <link type="text/css" rel="stylesheet" href="https://openai.com/assets/shared-1c21983e77ae4245f67f0a5135bd69bb60d588ed9df87a7cb74b25d467fecacb.css">
+    
+    
+    <link rel="icon" href="https://openai.com/assets/favicon-81f16c8d5fcc93e1b02354b72b40abb680deb71081fa467625154ccf471883dc.ico">
+    <link rel="shortcut icon" href="https://openai.com/assets/favicon-81f16c8d5fcc93e1b02354b72b40abb680deb71081fa467625154ccf471883dc.ico">
+    <link rel="apple-touch-icon" href="https://openai.com/assets/favicon-b52807aa8dda8e2badc657cccbdd1d44f388941a3c10522ba682098881d7c4ed.png">
+
+    <link rel="alternate" type="application/rss+xml" title="RSS" href="https://openai.com/feed.xml" />
+</head>
+
+  <body class=''>
+
+    
+
+    
+<?php include '../../header.html.php'; ?>
+
+
+<div class='Post'>
+<div class="container">
+  <div class="row
+              center-xs">
+
+  <div class="col-xs-12
+              col-sm-11
+              col-md-8
+              col-lg-8">
+      
+        <div class="Post-header">
+          <h1 class='Post-title'>Machine Learning Unconference</h1>
+          <div class='Post-meta'>
+            <span class="Post-author">Ian Goodfellow</span>
+            <span class="Post-date">18 de Agosto de 2016</span>
+          </div>
+        </div>      
+      
+        <p> <strong> As últimas informações sobre a Unconference estão disponíveis na <a href="https://github.com/openai/mlu16/wiki">Unconference wiki</a></strong>,
+que será atualizada periodicamente com mais informações para os participantes.
+</p>
+<hr />
+      
+<p> Machine learning está se movendo incrivelmente rápido. Para acompanhar, muitos praticantes passam várias semanas por ano em conferências. Contudo, as apresentações nas conferências são todas sobre trabalhos apresentados meses antes, o que significa que as pessoas já estão intimamente familiarizadas com o conteúdo (e muitas vezes já foi ultrapassado). </p>
+      
+      
+      
+<p>  Gostaríamos de tentar em vez disso, hospedar um evento focado na parte mais valiosa de qualquer conferência: as pessoas. Por favor, junte-se a nós para o nosso primeiro Machine Learning Unconference, um encontro experimental conduzido por seus participantes, em vez de um comitê organizador. </p>      
+      
+      
+<p>
+A <a href="https://en.wikipedia.org/wiki/Unconference">unconference</a>
+será um evento gratuito no escritório da OpenAI em San Francisco
+na <strong>sexta-feira e sábado, 7-8 de outubro de 2016</strong>. Serão bem vindos participantes de todo o mundo.
+A partir de 22 de agosto, nós terminamos de aceitar aplicações para a unconference.
+
+
+<h2> Por que ter uma unconference em machine learning? </h2>
+      
+<p> Conferências desempenham dois papéis principais: reunião social e publicação. Nós sentimos que estes dois papéis são um tanto ortogonais e podem ser melhor servidos separados. </p>
+      
+      
+      
+<p> Na maioria das conferências, o tempo social não estruturado, como as refeições, é o que as pessoas acham valioso. Queremos maximizar a quantidade de interatividade em uma reunião social de pesquisadores. Uma
+unconference minimamente estruturada fornece uma oportunidade para um experimento, para ver que tipos de novos formatos os participantes irão desenvolver e descobrir quais são os mais eficazes. </p>
+      
+      
+<p>  Também temos idéias sobre como melhorar o sistema de revisão e publicação de artigos, que estamos desenvolvendo separadamente. </p>
+
+<h2> Agenda </h2>
+      
+<p>  Seria ir contra o espírito da unconference se fornecermos uma agenda específica, mas sugerimos que os participantes usem
+o <a href="https://gitter.im/openai/mlu16">bate-papo Gitter</a> para planejar alguns tópicos de discussão com antecedencia. </p>
+
+<p>
+Todos os participantes também são incentivados a trazer um poster descrevendo um trabalho de machine
+learning que seria interessante ou útil para outros participantes aprenderem. (Seguiremos com mais detalhes sobre o formato por e-mail.)
+</p>
+
+<h2> Quem deve vir? </h2>
+      
+<p> Qualquer pessoa que esteja envolvida em pesquisas ou aplicações de machine learning é bem-vinda. Esperamos especialmente ver:</p>
+<ul>
+  <li> Estudantes de doutorado, membros de corpo docente e cientistas da indústria.</li>
+
+  <li> Engenheiros de software usando machine learning em aplicações ou construindo uma infra-estrutura de machine learning.</li>
+
+</ul>
+
+      
+<p> Estamos particularmente animados em apoiar mulheres, minorias e membros de outros grupos sub-representados em machine learning. A OpenAI fornecerá um número limitado de subsídios de viagem e alojamento para os membros destes grupos.
+</p>
+      
+
+<p>Este evento destina-se principalmente a pessoas com fluência técnica
+em machine learning para ajudar uns aos outros avançarem o estado da arte. Se você estiver interessado em aprender sobre o básico do campo, há uma abundância de outros <a href="http://ai.withthebest.com/">grandes</a> <a href="https://www.re-work.co/events/deep-learning-summit-san-francisco-2017">eventos</a>
+que sugerimos. </p>
+
+<h2> Logística </h2>
+      
+
+<p> A OpenAI está patrocinando e hospedando o evento, mas o evento em si não tem organização oficial &mdash; são apenas pessoas que acham este convite interessante, reunindo-se para discutir machine learning. </p>
+      
+<p> A OpenAI fornecerá almoço e lanches. Os participantes são responsáveis por organizar sua própria viagem e hospedagem. </p>
+
+      
+      
+<h2> Participantes </h2>
+
+<p> Ao participar, você concorda em seguir nosso <a href="https://openai.com/blog/machine-learning-unconference/code-of-conduct.txt">código de conduta</a>. </p>
+
+<p> Gostaríamos de dar as boas-vindas a todos, mas só temos capacidade para 150 pessoas. Ajustaremos a lista de aceitação para assegurar uma conferência interessante com uma diversidade de pessoas e perspectivas. </p>
+
+      
+<p> Aqui está uma amostra um pouco representativa de pessoas que você vai encontrar na unconference, com base em inscrições feitas com antecedência:
+</p>
+      
+<ul>
+  <li> <a href="http://conradomiranda.com/about/">Conrado Miranda</a>, estudante de doutorado, Universidade de Campinas</li>
+  <li> <a href="http://web.cs.ucla.edu/~feisha">Fei Sha</a>, Professor Associado, UCLA</li>
+  <li> <a href="https://people.eecs.berkeley.edu/~gkioxari">Georgia Gkioxari</a>, estudante de doutorado em Berkeley </li>
+  <li> <a href="http://www.cs.cmu.edu/~lwehbe/">Leila Wehbe</a>, Pesquisadora de Pós-Doutorado em Berkeley</li>
+  
+    
+    <li> Muitos membros do <a href="https://research.google.com/teams/brain/">Google
+  Brain</a>, incluindo <a href="http://bengio.abracadoudou.com/">Samy
+  Bengio</a>, <a href="http://research.google.com/pubs/GregCorrado.html">Greg
+  Corrado</a>,
+  <a href="http://research.google.com/pubs/author39086.html">Douglas Eck</a>,
+  e <a href="http://www.dumitru.ca/">Dumitru Erhan</a>.</li>
+    
+  <li> Muitos membros <a href="/blog/team-update-august/">da</a> <a href="/blog/team-update/">equipe</a> <a href="/blog/team-plus-plus/">da</a> <a href="/blog/introducing-openai/">OpenAI</a>. </li>
+    
+    
+  <li> <a href="http://memimo.net/">Mehdi Mirza</a>, estudante de doutorado, <a href="https://mila.umontreal.ca/en/">Montreal Institute for Learning Algorithms<a> </li>
+      
+  <li> <a href="https://www.papernot.fr/">Nicolas Papernot</a>, Google PhD Fellow em Segurança, Pennsylvania State University</li>
+      
+  <li> <a href="http://ai.stanford.edu/~syyeung/">Serena Yeung</a>, estudante de doutorado, Stanford University</li>
+      
+  <li> <a href="http://www.sudnya.net/">Sudnya Diamos</a>, Engenheiro de Ciências de dados e Algoritmos, <a href="http://attune.co/">Attune, Inc</a> </li>
+      
+  <li> <a href="http://research.google.com/pubs/ulfar.html">&Uacute;lfar Erlingsson</a>, responsável pelos esforços de segurança da Google Research </li>
+      
+  <li> <a href="http://www.wenlingshang.com/">Wendy Shang</a>, Engenheiro de Software, Oculus</li>
+      
+</ul>
+
+<hr />
+      
+<p> Você pode discutir planos e logística com outros participantes no <a href="https://gitter.im/openai/mlu16">bate-papo do Gitter</a>, ou enviar um e-mail <a href="mailto:ian@openai.com">Ian</a> com qualquer dúvida. </p>
+
+<div class='Post-Actions'>
+  <a href="https://gitter.im/openai/mlu16">Discutir logística</a>
+</div>
+
+        
+      </div>
+      
+    </div>
+  </div>
+</div>
+</div>
+
+    
+
+<?php include '../../scripts.html.php'; ?>
+    
+    
+    <!-- 1 -->
+  </body>
+</html>
